5d6bbbe9206ceb5149984f2081f3325db06ee775
unknown <fsiwinski05@gmail.com>
2024-01-11 08:50:23 +0100

    tak

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iosstream>
+#include <cstdlib>
+using namespace std;
+
+int main()
+{
+    constexpr int Rozmiar = 25;
+    int wyl_liczby[Rozmiar];
+    for(int i = 0; i < Rozmiar; i++){
+        int random = rand();0
+        wyl_liczby[i] = random;
+        cout<<wyl_liczby[i]<<" ";
+    }
+}
